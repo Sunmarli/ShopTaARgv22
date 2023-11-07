0a99280e6409c270ef9661e7ae83c2a8ba05d8b2
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2023-11-07 12:51:17 +0200

    on vaja weatercontroller method teha

---
+++ Shop/Shop/Controllers/OpenWeathersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopCore.ServiceInterface;
+
+namespace Shop.Controllers
+{
+	public class OpenWeathersController : Controller
+	{
+		private readonly IWeatherForecastServices _weatherForecastServices;
+		public OpenWeathersController(IWeatherForecastServices weatherForecastServices)
+		{
+			_weatherForecastServices = weatherForecastServices;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Shop/Shop/Models/OpenWeathers/SearchCityViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.OpenWeathers
+{
+    public class SearchCityViewModel
+    {
+        [Required(ErrorMessage ="You must enter city name")]
+        [RegularExpression("^[A-Za-z]+$",ErrorMessage ="Only text allowed")]
+        [StringLength(20,MinimumLength =2,ErrorMessage ="Enter a city name grather than 2 letters")]
+        [Display(Name ="City Name")]
+
+
+        public string CityName { get; set; }
+        
+    }
+}
