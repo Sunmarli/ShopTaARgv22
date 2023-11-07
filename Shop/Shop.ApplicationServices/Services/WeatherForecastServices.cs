ccf46abd432635335c6fdbc538360ef57a142342
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2023-11-07 10:23:51 +0200

    openweather class file

---
+++ Shop/Shop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,22 @@
+﻿using ShopCore.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.ApplicationServices.Services
+{
+	public class WeatherForecastServices
+	{
+		public async Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto)
+		{
+			string idOpenWeather = "4f6b9f481986443efd613140d2ba3440";
+			string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City}&units=metric&appid={idOpenWeather}";
+
+
+			return dto;
+		}
+
+	}
+}
