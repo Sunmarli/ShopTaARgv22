fe55d06b4858afc6a771779f02e19e81bd435650
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2023-12-12 10:46:55 +0200

    chat done

---
+++ Shop/Shop/Controllers/ChatHubController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+	public class ChatHubController : Controller
+	{
+		
+			public IActionResult Index()
+			{
+				return View();
+			}
+		
+	}
+}
--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -4,12 +4,13 @@ using ShopCore.ServiceInterface;
 using Shop.ApplicationServices.Services;
 using Microsoft.Extensions.FileProviders;
 using Shop.Data;
+using TARgv22Shop.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSignalR();
 builder.Services.AddDbContext<ShopContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -52,5 +53,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.MapHub<ChatHub>("/ChatHub/chat");
 
 app.Run();
