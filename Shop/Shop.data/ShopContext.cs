1096922958aef900dafcedd7705454752fcca0a5
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-09 09:27:25 +0200

    registration

--- Shop/Shop.data/ShopContext.cs
+++ Shop/Shop.data/ShopContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
-
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Core.Domain;
 using ShopCore.Domain;
 
 namespace Shop.Data
 {
-    public class ShopContext : DbContext
-    {
+	public class ShopContext : IdentityDbContext<ApplicationUser>
+	{
         public ShopContext
             (
                 DbContextOptions<ShopContext> options
---
+++ Shop/Shop/Models/Accounts/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Shop.Models.Accounts
+
+{
+	public class ForgotPasswordViewModel
+	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+	}
+}
